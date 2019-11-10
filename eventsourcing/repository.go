60ee7138623f25f7e1d2a78b15821d36dd21eb4f
Vaidotas Valuckas <vaidotas.valuckas@gmail.com>
2019-11-10 20:35:49 +0200

    value receivers for repository

--- eventsourcing/repository.go
+++ eventsourcing/repository.go
@@ -21,7 +21,7 @@ func (r Repository) newEventStream() *transactionalEventStream {
 	return NewEventStream(r.store, r.snapshotFrequency)
 }
 
-func (r *Repository) Query(id account.AggregateId) (*account.Snapshot, error) {
+func (r Repository) Query(id account.AggregateId) (*account.Snapshot, error) {
 	a := r.loadAggregate(id)
 	if a.err != nil {
 		return nil, a.err
@@ -31,19 +31,19 @@ func (r *Repository) Query(id account.AggregateId) (*account.Snapshot, error) {
 }
 
 // TODO: refactor to Create
-func (r *Repository) Open(id account.AggregateId, ownerId account.OwnerId) error {
+func (r Repository) Open(id account.AggregateId, ownerId account.OwnerId) error {
 	a := r.newAggregate(id)
 	return a.transact(func(a *account.Account) error {
 		return a.Open(id, ownerId)
 	})
 }
 
-func (r *Repository) Transact(id account.AggregateId, tx transaction) error {
+func (r Repository) Transact(id account.AggregateId, tx transaction) error {
 	a := r.loadAggregate(id)
 	return a.transact(tx)
 }
 
-func (r *Repository) BiTransact(sourceId, targetId account.AggregateId, tx biTransaction) error {
+func (r Repository) BiTransact(sourceId, targetId account.AggregateId, tx biTransaction) error {
 	es := r.newEventStream()
 	source, err := es.replay(sourceId)
 	if err != nil {
@@ -62,18 +62,12 @@ func (r *Repository) BiTransact(sourceId, targetId account.AggregateId, tx biTra
 	return es.commit()
 }
 
-func (r *Repository) aggregateExists(id account.AggregateId) bool {
+func (r Repository) aggregateExists(id account.AggregateId) bool {
 	events := r.store.Events(id, 0)
 	return len(events) != 0
 }
 
-type aggregate struct {
-	es  *transactionalEventStream
-	acc *account.Account
-	err error
-}
-
-func (r *Repository) newAggregate(id account.AggregateId) aggregate {
+func (r Repository) newAggregate(id account.AggregateId) aggregate {
 	a := aggregate{}
 	if r.aggregateExists(id) {
 		a.err = errors.New("account already exists")
@@ -85,11 +79,17 @@ func (r *Repository) newAggregate(id account.AggregateId) aggregate {
 	return a
 }
 
-func (r *Repository) loadAggregate(id account.AggregateId) aggregate {
+func (r Repository) loadAggregate(id account.AggregateId) aggregate {
 	a := aggregate{}
-	a.es = NewEventStream(r.store, r.snapshotFrequency)
+	a.es = r.newEventStream()
 	a.acc, a.err = a.es.replay(id)
 	return a
+}
+
+type aggregate struct {
+	es  *transactionalEventStream
+	acc *account.Account
+	err error
 }
 
 func (a *aggregate) transact(tx transaction) error {
