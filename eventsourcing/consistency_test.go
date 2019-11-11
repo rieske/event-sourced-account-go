696d6cffc360170c5f732b9faaf02c84b3ac53c1
Vaidotas Valuckas <vaidotas.valuckas@gmail.com>
2019-11-11 15:33:54 +0200

    refactor consistency tests

--- eventsourcing/consistency_test.go
+++ eventsourcing/consistency_test.go
@@ -8,8 +8,23 @@ import (
 )
 
 type consistencyTestFixture struct {
-	accountService accountService
-	aggregateId    account.Id
+	accountService  accountService
+	aggregateId     account.Id
+	operationCount  int
+	concurrentUsers int
+}
+
+func (f consistencyTestFixture) doConcurrently(t *testing.T, action func(s accountService) error) {
+	for i := 0; i < f.operationCount; i++ {
+		wg := sync.WaitGroup{}
+		for j := 0; j < f.concurrentUsers; j++ {
+			wg.Add(1)
+			go withRetryOnConcurrentModification(t, &wg, j, func() error {
+				return action(f.accountService)
+			})
+		}
+		wg.Wait()
+	}
 }
 
 func openAccount(t *testing.T, snapshottingFrequency int) *consistencyTestFixture {
@@ -19,7 +34,12 @@ func openAccount(t *testing.T, snapshottingFrequency int) *consistencyTestFixtur
 	err := accountService.OpenAccount(id, ownerId)
 	assert.NoError(t, err)
 
-	return &consistencyTestFixture{accountService, id}
+	return &consistencyTestFixture{
+		accountService:  accountService,
+		aggregateId:     id,
+		operationCount:  100,
+		concurrentUsers: 8,
+	}
 }
 
 func withRetryOnConcurrentModification(t *testing.T, wg *sync.WaitGroup, threadNo int, operation func() error) {
@@ -37,46 +57,22 @@ func withRetryOnConcurrentModification(t *testing.T, wg *sync.WaitGroup, threadN
 	wg.Done()
 }
 
-func TestConcurrentDeposits(t *testing.T) {
-	fixture := openAccount(t, 0)
+func testConcurrentDeposits(t *testing.T, snapshottingFrequency int) {
+	fixture := openAccount(t, snapshottingFrequency)
 
-	operationCount := 100
-	concurrentUsers := 8
-
-	for i := 0; i < operationCount; i++ {
-		wg := sync.WaitGroup{}
-		for j := 0; j < concurrentUsers; j++ {
-			wg.Add(1)
-			go withRetryOnConcurrentModification(t, &wg, j, func() error {
-				return fixture.accountService.Deposit(fixture.aggregateId, 1)
-			})
-		}
-		wg.Wait()
-	}
+	fixture.doConcurrently(t, func(s accountService) error {
+		return s.Deposit(fixture.aggregateId, 1)
+	})
 
 	snapshot, err := fixture.accountService.QueryAccount(fixture.aggregateId)
 	assert.NoError(t, err)
-	assert.Equal(t, int64(operationCount*concurrentUsers), snapshot.Balance)
+	assert.Equal(t, int64(fixture.operationCount*fixture.concurrentUsers), snapshot.Balance)
+}
+
+func TestConcurrentDeposits(t *testing.T) {
+	testConcurrentDeposits(t, 0)
 }
 
 func TestConcurrentDepositsWithSnapshotting(t *testing.T) {
-	fixture := openAccount(t, 5)
-
-	operationCount := 100
-	concurrentUsers := 8
-
-	for i := 0; i < operationCount; i++ {
-		wg := sync.WaitGroup{}
-		for j := 0; j < concurrentUsers; j++ {
-			wg.Add(1)
-			go withRetryOnConcurrentModification(t, &wg, j, func() error {
-				return fixture.accountService.Deposit(fixture.aggregateId, 1)
-			})
-		}
-		wg.Wait()
-	}
-
-	snapshot, err := fixture.accountService.QueryAccount(fixture.aggregateId)
-	assert.NoError(t, err)
-	assert.Equal(t, int64(operationCount*concurrentUsers), snapshot.Balance)
+	testConcurrentDeposits(t, 5)
 }
