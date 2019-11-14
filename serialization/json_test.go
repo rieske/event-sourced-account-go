820b779ae1a12b8d42066350ea2db4d009cce6dd
Vaidotas Valuckas <vaidotas.valuckas@gmail.com>
2019-11-14 10:01:49 +0400

    basic json serde test for reference

--- serialization/json_test.go
+++ serialization/json_test.go
@@ -8,26 +8,42 @@ import (
 	"testing"
 )
 
-func TestJsonSerializeAccountOpened(t *testing.T) {
-	// given
+type accountOpenedJsonTestFixture struct {
+	event           account.AccountOpenedEvent
+	serializedEvent []byte
+}
+
+func newAccountOpenedJsonTestFixture(t *testing.T) accountOpenedJsonTestFixture {
 	accountId, err := uuid.Parse("ce7d9c87-e348-406b-933b-0c6dfc0f014e")
 	assert.NoError(t, err)
 	ownerId, err := uuid.Parse("c2b0bbce-679a-4af5-9a75-8958da9eb02c")
 	assert.NoError(t, err)
 
-	event := account.AccountOpenedEvent{
-		AccountId: account.Id{accountId},
-		OwnerId:   account.OwnerId{ownerId},
+	return accountOpenedJsonTestFixture{
+		event: account.AccountOpenedEvent{
+			AccountId: account.Id{accountId},
+			OwnerId:   account.OwnerId{ownerId},
+		},
+		serializedEvent: []byte(`{"AccountId":"ce7d9c87-e348-406b-933b-0c6dfc0f014e","OwnerId":"c2b0bbce-679a-4af5-9a75-8958da9eb02c"}`),
 	}
+}
 
-	// when
-	serialized, err := json.Marshal(event)
+func TestJsonSerializeAccountOpened(t *testing.T) {
+	fixture := newAccountOpenedJsonTestFixture(t)
+
+	serializedEvent, err := json.Marshal(fixture.event)
+
+	assert.NoError(t, err)
+	assert.Equal(t, fixture.serializedEvent, serializedEvent)
+}
+
+func TestJsonDeserializeAccountOpened(t *testing.T) {
+	fixture := newAccountOpenedJsonTestFixture(t)
+
+	var event account.AccountOpenedEvent
+	err := json.Unmarshal(fixture.serializedEvent, &event)
 
 	// then
 	assert.NoError(t, err)
-	assert.Equal(
-		t,
-		`{"AccountId":"ce7d9c87-e348-406b-933b-0c6dfc0f014e","OwnerId":"c2b0bbce-679a-4af5-9a75-8958da9eb02c"}`,
-		string(serialized[:]),
-	)
+	assert.Equal(t, fixture.event, event)
 }
