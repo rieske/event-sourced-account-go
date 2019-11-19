771037fae1dc1fa72c2e0c3268f813e3cf466f0c
Vaidotas Valuckas <vaidotas.valuckas@gmail.com>
2019-11-19 15:55:26 +0200

    refactored rest tests

--- rest/account_resource_test.go
+++ rest/account_resource_test.go
@@ -10,213 +10,216 @@ import (
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"testing"
 )
 
-var server = rest.NewRestServer(eventstore.NewInMemoryStore(), 0)
+type accountResourceFixture struct {
+	assert.Assertions
+	server *rest.Server
+}
 
-func createAccount(t *testing.T, accountId account.Id, ownerId account.OwnerId) string {
-	req, err := http.NewRequest("POST", "/account/"+accountId.String()+"?owner="+ownerId.String(), nil)
-	assert.NoError(t, err)
+func newFixture(t *testing.T) accountResourceFixture {
+	return accountResourceFixture{
+		Assertions: *assert.New(t),
+		server:     rest.NewRestServer(eventstore.NewInMemoryStore(), 0),
+	}
+}
+
+func (f accountResourceFixture) post(path string) *httptest.ResponseRecorder {
+	req, err := http.NewRequest(http.MethodPost, path, nil)
+	f.NoError(err)
 	recorder := httptest.NewRecorder()
 
-	server.ServeHTTP(recorder, req)
+	f.server.ServeHTTP(recorder, req)
 
-	assert.Equal(t, http.StatusCreated, recorder.Code)
-	assert.Equal(t, "", recorder.Body.String())
+	return recorder
+}
+
+func (f accountResourceFixture) get(path string) *httptest.ResponseRecorder {
+	req, err := http.NewRequest(http.MethodGet, path, nil)
+	f.NoError(err)
+	recorder := httptest.NewRecorder()
+
+	f.server.ServeHTTP(recorder, req)
+
+	return recorder
+}
+
+func (f accountResourceFixture) put(path string) *httptest.ResponseRecorder {
+	req, err := http.NewRequest(http.MethodPut, path, nil)
+	f.NoError(err)
+	recorder := httptest.NewRecorder()
+
+	f.server.ServeHTTP(recorder, req)
+
+	return recorder
+}
+
+func (f accountResourceFixture) createAccount(accountId account.Id, ownerId account.OwnerId) string {
+	req, err := http.NewRequest("POST", "/account/"+accountId.String()+"?owner="+ownerId.String(), nil)
+	f.NoError(err)
+	recorder := httptest.NewRecorder()
+
+	f.server.ServeHTTP(recorder, req)
+
+	f.Equal(http.StatusCreated, recorder.Code)
+	f.Equal("", recorder.Body.String())
 	return recorder.Header().Get("Location")
 }
 
-func queryAccount(t *testing.T, accountId account.Id) account.Snapshot {
+func (f accountResourceFixture) queryAccount(accountId account.Id) account.Snapshot {
 	req, err := http.NewRequest("GET", "/account/"+accountId.String(), nil)
-	assert.NoError(t, err)
+	f.NoError(err)
 	recorder := httptest.NewRecorder()
 
-	server.ServeHTTP(recorder, req)
+	f.server.ServeHTTP(recorder, req)
 
-	assert.Equal(t, "application/json", recorder.Header().Get("Content-Type"))
-	assert.Equal(t, http.StatusOK, recorder.Code)
+	f.Equal("application/json", recorder.Header().Get("Content-Type"))
+	f.Equal(http.StatusOK, recorder.Code)
 
 	var snapshot account.Snapshot
 	err = json.Unmarshal(recorder.Body.Bytes(), &snapshot)
-	assert.NoError(t, err)
+	f.NoError(err)
 	return snapshot
 }
 
+func (f accountResourceFixture) deposit(accountId account.Id, amount int64, txId uuid.UUID) {
+	res := f.put("/account/" + accountId.String() + "/deposit?amount=" + strconv.FormatInt(amount, 10) + "&transactionId=" + txId.String())
+	f.Equal(http.StatusNoContent, res.Code)
+}
+
 func TestOpenAccount(t *testing.T) {
+	f := newFixture(t)
 	accountId, ownerId := account.NewId(), account.NewOwnerId()
-	createAccount(t, accountId, ownerId)
+	f.createAccount(accountId, ownerId)
 }
 
 func TestRequireValidUUIDForAccountId(t *testing.T) {
-	ownerId := account.NewOwnerId()
-	req, err := http.NewRequest("POST", "/account/foobar?owner="+ownerId.String(), nil)
-	assert.NoError(t, err)
-	recorder := httptest.NewRecorder()
+	f := newFixture(t)
 
-	server.ServeHTTP(recorder, req)
+	res := f.post("/account/foobar?owner=" + account.NewOwnerId().String())
 
-	assert.Equal(t, http.StatusBadRequest, recorder.Code)
-	assert.Equal(t, `{"message":"Invalid UUID string: foobar"}`, recorder.Body.String())
+	assert.Equal(t, http.StatusBadRequest, res.Code)
+	assert.Equal(t, `{"message":"Invalid UUID string: foobar"}`, res.Body.String())
 }
 
 func TestRequireValidUUIDForOwnerId(t *testing.T) {
-	accountId := account.NewId()
-	req, err := http.NewRequest("POST", "/account/"+accountId.String()+"?owner=foobar", nil)
-	assert.NoError(t, err)
-	recorder := httptest.NewRecorder()
+	f := newFixture(t)
 
-	server.ServeHTTP(recorder, req)
+	res := f.post("/account/" + account.NewId().String() + "?owner=foobar")
 
-	assert.Equal(t, http.StatusBadRequest, recorder.Code)
-	assert.Equal(t, `{"message":"Invalid UUID string: foobar"}`, recorder.Body.String())
+	assert.Equal(t, http.StatusBadRequest, res.Code)
+	assert.Equal(t, `{"message":"Invalid UUID string: foobar"}`, res.Body.String())
 }
 
 func TestConflictOnAccountOpeningWhenAccountAlreadyExists(t *testing.T) {
+	f := newFixture(t)
 	accountId := account.NewId()
-	createAccount(t, accountId, account.NewOwnerId())
+	f.createAccount(accountId, account.NewOwnerId())
 
-	req, err := http.NewRequest("POST", "/account/"+accountId.String()+"?owner="+uuid.New().String(), nil)
-	assert.NoError(t, err)
-	recorder := httptest.NewRecorder()
+	res := f.post("/account/" + accountId.String() + "?owner=" + uuid.New().String())
 
-	server.ServeHTTP(recorder, req)
-
-	assert.Equal(t, http.StatusConflict, recorder.Code)
-	assert.Equal(t, `{"message":"account already exists"}`, recorder.Body.String())
+	assert.Equal(t, http.StatusConflict, res.Code)
+	assert.Equal(t, `{"message":"account already exists"}`, res.Body.String())
 }
 
 func TestQueryAccount(t *testing.T) {
+	f := newFixture(t)
 	accountId, ownerId := account.NewId(), account.NewOwnerId()
-	resource := createAccount(t, accountId, ownerId)
+	resource := f.createAccount(accountId, ownerId)
 
-	req, err := http.NewRequest("GET", resource, nil)
-	assert.NoError(t, err)
-	recorder := httptest.NewRecorder()
+	res := f.get(resource)
 
-	server.ServeHTTP(recorder, req)
-
-	assert.Equal(t, http.StatusOK, recorder.Code)
-	assert.Equal(t, "application/json", recorder.Header().Get("Content-Type"))
+	assert.Equal(t, http.StatusOK, res.Code)
+	assert.Equal(t, "application/json", res.Header().Get("Content-Type"))
 	assert.Equal(t,
 		fmt.Sprintf(
 			`{"Id":"%s","OwnerId":"%s","Balance":0,"Open":true}`,
 			accountId.String(), ownerId.String()),
-		recorder.Body.String(),
+		res.Body.String(),
 	)
 }
 
 func Test404WhenQueryingNonExistentAccount(t *testing.T) {
+	f := newFixture(t)
 	accountId := account.NewId()
 
-	req, err := http.NewRequest("GET", "/account/"+accountId.String(), nil)
-	assert.NoError(t, err)
-	recorder := httptest.NewRecorder()
+	res := f.get("/account/" + accountId.String())
 
-	server.ServeHTTP(recorder, req)
-
-	assert.Equal(t, http.StatusNotFound, recorder.Code)
-	assert.Equal(t, "application/json", recorder.Header().Get("Content-Type"))
-	assert.Equal(t, `{"message":"account not found"}`, recorder.Body.String())
+	assert.Equal(t, http.StatusNotFound, res.Code)
+	assert.Equal(t, "application/json", res.Header().Get("Content-Type"))
+	assert.Equal(t, `{"message":"account not found"}`, res.Body.String())
 }
 
 func TestDepositMoney(t *testing.T) {
+	f := newFixture(t)
 	accountId := account.NewId()
-	createAccount(t, accountId, account.NewOwnerId())
+	f.createAccount(accountId, account.NewOwnerId())
 
-	req, err := http.NewRequest("PUT", "/account/"+accountId.String()+"/deposit?amount=42&transactionId="+uuid.New().String(), nil)
-	assert.NoError(t, err)
-	recorder := httptest.NewRecorder()
+	f.deposit(accountId, 42, uuid.New())
 
-	server.ServeHTTP(recorder, req)
-
-	assert.Equal(t, http.StatusNoContent, recorder.Code)
-
-	snapshot := queryAccount(t, accountId)
+	snapshot := f.queryAccount(accountId)
 	assert.Equal(t, int64(42), snapshot.Balance)
 }
 
 func TestDepositsAccumulateBalance(t *testing.T) {
+	f := newFixture(t)
 	accountId := account.NewId()
-	createAccount(t, accountId, account.NewOwnerId())
+	f.createAccount(accountId, account.NewOwnerId())
 
-	req, err := http.NewRequest("PUT", "/account/"+accountId.String()+"/deposit?amount=42&transactionId="+uuid.New().String(), nil)
-	assert.NoError(t, err)
-	recorder := httptest.NewRecorder()
-	server.ServeHTTP(recorder, req)
-	assert.Equal(t, http.StatusNoContent, recorder.Code)
+	f.deposit(accountId, 42, uuid.New())
+	f.deposit(accountId, 42, uuid.New())
 
-	req, err = http.NewRequest("PUT", "/account/"+accountId.String()+"/deposit?amount=42&transactionId="+uuid.New().String(), nil)
-	assert.NoError(t, err)
-	server.ServeHTTP(recorder, req)
-	assert.Equal(t, http.StatusNoContent, recorder.Code)
-
-	snapshot := queryAccount(t, accountId)
+	snapshot := f.queryAccount(accountId)
 	assert.Equal(t, int64(84), snapshot.Balance)
 }
 
 func TestDepositsAreIdempotent(t *testing.T) {
+	f := newFixture(t)
 	accountId := account.NewId()
-	createAccount(t, accountId, account.NewOwnerId())
+	f.createAccount(accountId, account.NewOwnerId())
 
 	txId := uuid.New()
+	f.deposit(accountId, 42, txId)
+	f.deposit(accountId, 42, txId)
 
-	req, err := http.NewRequest("PUT", "/account/"+accountId.String()+"/deposit?amount=42&transactionId="+txId.String(), nil)
-	assert.NoError(t, err)
-	recorder := httptest.NewRecorder()
-	server.ServeHTTP(recorder, req)
-	assert.Equal(t, http.StatusNoContent, recorder.Code)
-
-	req, err = http.NewRequest("PUT", "/account/"+accountId.String()+"/deposit?amount=42&transactionId="+txId.String(), nil)
-	assert.NoError(t, err)
-	server.ServeHTTP(recorder, req)
-	assert.Equal(t, http.StatusNoContent, recorder.Code)
-
-	snapshot := queryAccount(t, accountId)
+	snapshot := f.queryAccount(accountId)
 	assert.Equal(t, int64(42), snapshot.Balance)
 }
 
 func TestDoNotAcceptFloatingPointDeposit(t *testing.T) {
+	f := newFixture(t)
 	accountId := account.NewId()
-	createAccount(t, accountId, account.NewOwnerId())
+	f.createAccount(accountId, account.NewOwnerId())
 
-	req, err := http.NewRequest("PUT", "/account/"+accountId.String()+"/deposit?amount=42.4&transactionId="+uuid.New().String(), nil)
-	assert.NoError(t, err)
-	recorder := httptest.NewRecorder()
+	res := f.put("/account/" + accountId.String() + "/deposit?amount=42.4&transactionId=" + uuid.New().String())
 
-	server.ServeHTTP(recorder, req)
-
-	assert.Equal(t, http.StatusBadRequest, recorder.Code)
-	assert.Equal(t, "application/json", recorder.Header().Get("Content-Type"))
-	assert.Equal(t, `{"message":"integer amount required, got '42.4'"}`, recorder.Body.String())
+	assert.Equal(t, http.StatusBadRequest, res.Code)
+	assert.Equal(t, "application/json", res.Header().Get("Content-Type"))
+	assert.Equal(t, `{"message":"integer amount required, got '42.4'"}`, res.Body.String())
 }
 
 func TestDoNotAcceptNonNumericDeposit(t *testing.T) {
+	f := newFixture(t)
 	accountId := account.NewId()
-	createAccount(t, accountId, account.NewOwnerId())
+	f.createAccount(accountId, account.NewOwnerId())
 
-	req, err := http.NewRequest("PUT", "/account/"+accountId.String()+"/deposit?amount=banana&transactionId="+uuid.New().String(), nil)
-	assert.NoError(t, err)
-	recorder := httptest.NewRecorder()
+	res := f.put("/account/" + accountId.String() + "/deposit?amount=banana&transactionId=" + uuid.New().String())
 
-	server.ServeHTTP(recorder, req)
-
-	assert.Equal(t, http.StatusBadRequest, recorder.Code)
-	assert.Equal(t, "application/json", recorder.Header().Get("Content-Type"))
-	assert.Equal(t, `{"message":"integer amount required, got 'banana'"}`, recorder.Body.String())
+	assert.Equal(t, http.StatusBadRequest, res.Code)
+	assert.Equal(t, "application/json", res.Header().Get("Content-Type"))
+	assert.Equal(t, `{"message":"integer amount required, got 'banana'"}`, res.Body.String())
 }
 
 func TestDoNotAcceptNegativeDeposit(t *testing.T) {
+	f := newFixture(t)
 	accountId := account.NewId()
-	createAccount(t, accountId, account.NewOwnerId())
+	f.createAccount(accountId, account.NewOwnerId())
 
-	req, err := http.NewRequest("PUT", "/account/"+accountId.String()+"/deposit?amount=-1&transactionId="+uuid.New().String(), nil)
-	assert.NoError(t, err)
-	recorder := httptest.NewRecorder()
+	res := f.put("/account/" + accountId.String() + "/deposit?amount=-1&transactionId=" + uuid.New().String())
 
-	server.ServeHTTP(recorder, req)
-
-	assert.Equal(t, http.StatusBadRequest, recorder.Code)
-	assert.Equal(t, "application/json", recorder.Header().Get("Content-Type"))
-	assert.Equal(t, `{"message":"can not deposit negative amount"}`, recorder.Body.String())
+	assert.Equal(t, http.StatusBadRequest, res.Code)
+	assert.Equal(t, "application/json", res.Header().Get("Content-Type"))
+	assert.Equal(t, `{"message":"can not deposit negative amount"}`, res.Body.String())
 }
