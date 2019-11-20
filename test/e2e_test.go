0af41adbac1fbda8101bf0663df096d2c9fabf49
Vaidotas Valuckas <vaidotas.valuckas@gmail.com>
2019-11-20 18:03:48 +0200

    simple e2e test creating and querying an account

--- test/e2e_test.go
+++ test/e2e_test.go
@@ -3,7 +3,10 @@
 package test
 
 import (
+	"fmt"
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -60,8 +63,30 @@ func TestPing(t *testing.T) {
 	res, err := http.Get(serviceUrl + "/ping")
 
 	assert.NoError(t, err)
+	require.Equal(t, http.StatusOK, res.StatusCode)
+	body, err := ioutil.ReadAll(res.Body)
+	require.NoError(t, err)
+	assert.Equal(t, "pong", string(body))
+}
+
+func TestCreateAndQueryAccount(t *testing.T) {
+	accountID, ownerID := uuid.New().String(), uuid.New().String()
+
+	res, err := http.Post(serviceUrl+"/account/"+accountID+"?owner="+ownerID, "", nil)
+	require.NoError(t, err)
+	assert.Equal(t, http.StatusCreated, res.StatusCode)
+
+	accountLocation, err := res.Location()
+	require.NoError(t, err)
+
+	res, err = http.Get(accountLocation.String())
+	require.NoError(t, err)
 	assert.Equal(t, http.StatusOK, res.StatusCode)
 	body, err := ioutil.ReadAll(res.Body)
-	assert.NoError(t, err)
-	assert.Equal(t, "pong", string(body))
+	require.NoError(t, err)
+	assert.Equal(
+		t,
+		fmt.Sprintf(`{"ID":"%s","OwnerID":"%s","Balance":0,"Open":true}`, accountID, ownerID),
+		string(body),
+	)
 }
